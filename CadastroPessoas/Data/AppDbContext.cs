9f00677374b3b5028708b0f97752abd123b7eb30
matheus3746 <coutom580@gmail.com>
2024-07-31 14:07:38 -0300

    Add files via upload

---
+++ CadastroPessoas/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CadastroPessoas.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CadastroPessoas.Data;
+
+public class AppDbContext:DbContext
+{
+	public AppDbContext(DbContextOptions options) : base(options)
+	{
+
+	}
+    DbSet<Pessoa> Pessoas { get; set; } = null!;
+}
